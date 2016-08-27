ed5f3636e512766121aaf1e15c716b67dc6ef8dd
Oliver Davey <oliver.davey@callvision-tech.com>
2016-08-27 23:00:07 +0100

    Writes to stream and joins channel

--- client.rs
+++ client.rs
@@ -1,11 +1,23 @@
 use std::net::TcpStream;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Write, BufWriter};
 
 fn main()
 {
 	let stream = TcpStream::connect("irc.freenode.org:6667").unwrap();
 	let mut line = String::new();
-	let mut reader = BufReader::new(stream);
+	let mut reader = BufReader::new(&stream);
+	let mut writer = BufWriter::new(&stream);
+	
+	let _ = writer.write(b"NICK aapple\r\n");
+	let _ = writer.write(b"USER aapple tolmoon tolsun :An Apple\r\n");
+	let _ = writer.write(b"JOIN ##rust\r\n");
+
+	let sent = writer.flush();
+	match sent {
+		Ok(v) => println!("{:?}", v),
+		Err(e) => println!("{:?}", e),
+	}
+	
 	loop {
 		let _ = reader.read_line( &mut line);
         println!("{}", line);
